c4c27be803cd47d6e24876c4231b88474b3dea50
Francisco Castillo <fcastillocorpus@gmail.com>
2025-05-13 13:23:45 -0600

    added color palette

--- lib/constantes.dart
+++ lib/constantes.dart
@@ -1,1 +1,9 @@
+import 'dart:ui';
+
 String apiKey = '87bb09d58a576a6647d91f26b6e81c0f'; 
+
+//Paleta de colores
+const backgroundColor = Color(0xff2C2C2C);
+const purpleBroker = Color(0xff8075FF);
+const grayMargins = Color(0xff868784);
+const yellowBroker = Color(0xffF0C808);
